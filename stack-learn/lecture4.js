508e8ec13582259a3d5afbdb5dd68c505bff7f6a
mdshohaan <irfanulislamdu@gmail.com>
2025-03-05 00:27:33 +0600

    create new array

---
+++ stack-learn/lecture4.js
@@ -0,0 +1,12 @@
+//  @title:update array elemets and fill array
+
+//  Array fill manual way
+const arr = new Array(10);
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = false;
+}
+// console.log(arr);
+
+//  array fill
+arr.fill(true);
+// console.log(arr);
