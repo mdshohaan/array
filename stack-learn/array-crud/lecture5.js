7caa12ca3dbedc9ae5c77d7c424ea625a07a35f0
mdshohaan <irfanulislamdu@gmail.com>
2025-03-07 00:37:12 +0600

    array add

--- stack-learn/array-crud/lecture5.js
+++ stack-learn/array-crud/lecture5.js
@@ -1,3 +1,14 @@
 /** Slice Array into Multiple Array
  *
  */
+const arr = [1, 2, 3, 4, 5, 6];
+const sliced = arr.slice(1, 3); // inclusive and exclusive
+// Clone Array
+const cloned = arr.slice();
+
+// Array like objects to Array
+function toArray() {
+  return Array.prototype.slice.call(arguments);
+}
+const argArray = toArray(1, 3, 5, "test");
+console.log(argArray);
