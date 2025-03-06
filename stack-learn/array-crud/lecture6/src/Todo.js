3746f529ca1583938eb77364f40c64af5d28eb3a
mdshohaan <irfanulislamdu@gmail.com>
2025-03-07 01:56:16 +0600

    add the projects

---
+++ stack-learn/array-crud/lecture6/src/Todo.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr
+ */
+
+function generateID(arr) {
+  if (arr.length === o) return 1;
+  return arr[arr.length - 1].id + 1;
+}
+/**
+ * @property todoList
+ * @method addItem
+ * @method update
+ * @method done
+ * @method next
+ * @method find
+ * @method list
+ */
--- stack-learn/index.js
+++ stack-learn/index.js
@@ -1,8 +1,10 @@
 // Length = lastIndex + 1
 // LastIndex = Length - 1
-// array notation
+
+//  array notation
 const number = [1, 2, 3, 4, 5];
 number[5] = 6; // but this not fair bcz of when we add in an element in dynamic array then we got problem where index or length
+
 number[number.length] = 6; // same output. this is best practise
 
 console.log(number);
