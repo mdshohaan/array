ee3c69ca2c414eb9e46c5bdd796c9ab455ab8413
mdshohaan <irfanulislamdu@gmail.com>
2025-03-08 23:15:45 +0600

    Explore more stack

--- stack-learn/array-as-a-stack/Traversy/Stack.js
+++ stack-learn/array-as-a-stack/Traversy/Stack.js
@@ -19,10 +19,28 @@ class Stack {
     console.log(`Deleted Item ${deletedItem}`);
     return deletedItem;
   }
+  // check the Top Element
+  peek() {
+    console.log(`Top elemnt is ${this.items[this.count - 1]}`);
+    return this.items[this.count - 1];
+  }
+  // Check Stack is Empty
+  isEmpty() {
+    console.log(this.count === 0 ? "Stack is empty" : "Stack is Not empty");
+    return this.count === 0;
+  }
+  size() {
+    console.log(`Stack is size ${this.count}`);
+    return this.count;
+  }
 }
 const stack = new Stack();
+stack.isEmpty();
 stack.push(20);
 stack.push(30);
 stack.push(30);
 stack.pop();
+stack.peek();
 stack.pop();
+stack.size();
+stack.isEmpty();
