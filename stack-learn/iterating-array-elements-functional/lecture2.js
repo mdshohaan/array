7a506a56bafeb747615fabd369f40455efaf7327
mdshohaan <irfanulislamdu@gmail.com>
2025-03-09 20:27:20 +0600

    Add more elements

--- stack-learn/iterating-array-elements-functional/lecture2.js
+++ stack-learn/iterating-array-elements-functional/lecture2.js
@@ -1,3 +1,4 @@
+//  Map Method Related
 const names = [
   "Stack learner",
   "Stack school",
@@ -15,7 +16,7 @@ const result1 = [];
 reuse(names, (value) => {
   return result1.push(value.length);
 });
-console.log(result1);
+// console.log(result1);
 
 // Iterating like Map function
 function resueAndMap(arr, logic) {
@@ -27,5 +28,29 @@ function resueAndMap(arr, logic) {
 }
 const lengths = resueAndMap(names, (value) => value.length);
 const uppercase = resueAndMap(names, (value) => value.toUpperCase());
-console.log(lengths);
-console.log(uppercase);
+// console.log(lengths);
+// console.log(uppercase);
+
+// Javascript Build in Map Method
+const lenghts1 = names.map((value) => value.length);
+// console.log("Build in Method", lenghts1);
+
+const students = [
+  { id: 1, name: "Alice Johnson", gpa: 3.8, email: "alice@example.com" },
+  { id: 2, name: "Bob Smith", gpa: 3.5, email: "bob@example.com" },
+  { id: 3, name: "Charlie Brown", gpa: 3.9, email: "charlie@example.com" },
+  { id: 4, name: "David Williams", gpa: 3.2, email: "david@example.com" },
+  { id: 5, name: "Emma Wilson", gpa: 3.7, email: "emma@example.com" },
+];
+const mappedStudents = students.map((value) => {
+  return {
+    ...value,
+    Title: `Hello ${value.name} Your Result has been Published`,
+    msg: `You have got ${value.gpa >= 3.5 ? "Passed" : "Failed"}`,
+  };
+});
+// console.log(mappedStudents);
+const mappedStudents1 = students.map((value) => {
+  return value.gpa >= 3.5 ? "Passed" : "Failed";
+});
+console.log(mappedStudents1);
