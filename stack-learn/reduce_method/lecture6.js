66b8a924c44d9758890e2f797accdec3bcdef9ba
mdshohaan <irfanulislamdu@gmail.com>
2025-03-10 22:23:55 +0600

    Chore:Reduce method implementation

--- stack-learn/reduce_method/lecture6.js
+++ stack-learn/reduce_method/lecture6.js
@@ -2,18 +2,19 @@
  *
  */
 function myReduce(arr, cb, init) {
-  let acc = init;
-  for (let i = 0; i < arr.length; i++) {
+  let acc = init,
+    start = 0;
+  if (!init) {
+    acc = arr[0];
+    start = 1;
+  }
+  for (let i = start; i < arr.length; i++) {
     acc = cb(acc, arr[i], i, arr);
   }
   return acc;
 }
 const arr = [1, 2, 3, 4, 5, 6];
-const sum = myReduce(
-  arr,
-  (acc, curr) => {
-    return acc + curr;
-  },
-  0
-);
+const sum = myReduce(arr, (acc, curr) => {
+  return acc + curr;
+});
 console.log(sum);
