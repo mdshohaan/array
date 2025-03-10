a61db0c80260ceae68ebae3a12546255bcd74438
mdshohaan <irfanulislamdu@gmail.com>
2025-03-10 22:08:42 +0600

    Add more reduce functionality

--- stack-learn/reduce_method/lecture6.js
+++ stack-learn/reduce_method/lecture6.js
@@ -1,8 +1,19 @@
 /** Implement Our Own Reduce
  *
  */
-function myReduce(arr, init) {
+function myReduce(arr, cb, init) {
   let acc = init;
-  for (let i = 0; i < arr.length; i++) {}
+  for (let i = 0; i < arr.length; i++) {
+    acc = cb(acc, arr[i], i, arr);
+  }
   return acc;
 }
+const arr = [1, 2, 3, 4, 5, 6];
+const sum = myReduce(
+  arr,
+  (acc, curr) => {
+    return acc + curr;
+  },
+  0
+);
+console.log(sum);
