513640348bd88d3339aa6730d76e91c0dcf2581d
mdshohaan <irfanulislamdu@gmail.com>
2025-03-11 07:06:36 +0600

    Add more reduce methods

--- stack-learn/reduce_method/lecture8.js
+++ stack-learn/reduce_method/lecture8.js
@@ -4,8 +4,14 @@ const matrix = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-// Vertical
+// Horizontal sum
 const sumH = matrix.map((row) => {
   return row.reduce((a, b) => a + b);
 });
 console.log(sumH);
+
+// Vertical Sum
+const sumV = matrix.reduce((acc, curr) => {
+  return acc.map((v, i) => curr[i] + v);
+});
+console.log(sumV);
