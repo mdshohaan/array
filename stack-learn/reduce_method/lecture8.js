fa39eb803774c871be974341ef8972c4188847f2
mdshohaan <irfanulislamdu@gmail.com>
2025-03-12 05:25:44 +0600

    Add new Array

--- stack-learn/reduce_method/lecture8.js
+++ stack-learn/reduce_method/lecture8.js
@@ -12,6 +12,21 @@ console.log(sumH);
 
 // Vertical Sum
 const sumV = matrix.reduce((acc, curr) => {
+  console.log("ACC:", acc, "CURR:", curr);
   return acc.map((v, i) => curr[i] + v);
 });
 console.log(sumV);
+// •	v = 1, i = 0, so curr[0] + v = 4 + 1 = 5
+// •	v = 2, i = 1, so curr[1] + v = 5 + 2 = 7
+// •	v = 3, i = 2, so curr[2] + v = 6 + 3 = 9
+
+// So, after this map, we get:
+//   acc = [5, 7, 9]
+
+// •	v = 5, i = 0, so curr[0] + v = 7 + 5 = 12
+// •	v = 7, i = 1, so curr[1] + v = 8 + 7 = 15
+// •	v = 9, i = 2, so curr[2] + v = 9 + 9 = 18
+
+// •	map is used to sum corresponding elements from two arrays.
+// •	Inside reduce, acc keeps updating as we go through each row.
+// •	map ensures that elements in each column are added together.
