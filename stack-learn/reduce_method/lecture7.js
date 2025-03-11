c70593c00b8a2d16e433ff5694b3519414ed1096
mdshohaan <irfanulislamdu@gmail.com>
2025-03-11 06:40:06 +0600

    Chore:Add more elemnts

--- stack-learn/reduce_method/lecture7.js
+++ stack-learn/reduce_method/lecture7.js
@@ -1,3 +1,29 @@
-/** Reduce Right
+/** Reduce Array into Single Object
  *
  */
+const products = [
+  { name: "Laptop", price: 800 },
+  { name: "Smartphone", price: 500 },
+  { name: "Tablet", price: 300 },
+  { name: "Tablet", price: 300 },
+  { name: "Smartwatch", price: 250 },
+  { name: "Keyboard", price: 50 },
+  { name: "Monitor", price: 200 },
+  { name: "Keyboard", price: 50 },
+  { name: "Gaming Chair", price: 150 },
+  { name: "Laptop", price: 800 },
+];
+
+const invoice = products.reduce((acc, curr) => {
+  if (acc[curr.name]) {
+    acc[curr.name].Quantity++;
+    acc[curr.name].price += curr.price;
+  } else {
+    acc[curr.name] = {
+      price: curr.price,
+      Quantity: 1,
+    };
+  }
+  return acc;
+}, {});
+console.log(invoice);
