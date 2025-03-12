e774328b62b63a38cdcc37912869b8e3b6566c38
mdshohaan <irfanulislamdu@gmail.com>
2025-03-12 21:38:41 +0600

    Add the data

---
+++ stack-learn/reduce_method/lecture11.js
@@ -0,0 +1,50 @@
+//  Inspect Nested Object
+
+const js = {
+  name: "All you need to know javascript",
+  author: {
+    name: "Irfan",
+    email: "abc@gmail.com",
+  },
+  contents: {
+    video: {
+      count: 222,
+    },
+  },
+};
+const react = {
+  name: "All you need to know javascript",
+  author: {
+    name: "Irfan",
+    email: "abc@gmail.com",
+  },
+  contents: {
+    video: {
+      count: 222,
+    },
+    article: {
+      count: 25,
+    },
+    quiz: {
+      count: 10,
+    },
+  },
+};
+const java = {
+  name: "All you need to know javascript",
+  author: {
+    name: "Irfan",
+    email: "abc@gmail.com",
+  },
+  contents: {
+    video: {
+      count: 222,
+    },
+    article: {
+      count: 25,
+    },
+    quiz: {
+      count: 10,
+    },
+  },
+};
