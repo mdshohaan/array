9f524c6e3885981fc798203cc03b2364cc6522af
mdshohaan <irfanulislamdu@gmail.com>
2025-03-15 04:51:22 +0600

    create localcompare method

--- stack-learn/sort-and-search/localComapre.js
+++ stack-learn/sort-and-search/localComapre.js
@@ -5,6 +5,15 @@ console.log("apple".localeCompare("banana")); // Output: -1 (apple comes before 
 console.log("grape".localeCompare("banana")); // Output: 1 (grape comes after banana)
 console.log("orange".localeCompare("orange")); // Output: 0 (both are equal)
 
+// The letter "a" is before "c" yielding a negative value
+"a".localeCompare("c"); // -2 or -1 (or some other negative value)
+
+// Alphabetically the word "check" comes after "against" yielding a positive value
+"check".localeCompare("against"); // 2 or 1 (or some other positive value)
+
+// "a" and "a" are equivalent yielding a neutral value of zero
+"a".localeCompare("a"); // 0
+
 const fruits = ["banana", "apple", "grape", "orange"];
 fruits.sort((a, b) => a.localeCompare(b));
 
