e1d1e9e9bf3f66abae4e45eab53962ba75e00b61
mdshohaan <irfanulislamdu@gmail.com>
2025-03-15 11:59:27 +0600

    Array module over all

---
+++ stack-learn/other-array-methods/lecture2.js
@@ -0,0 +1,12 @@
+// Includes,IndexOf,LastIndexOf
+
+const arr = [1, 2, 3, 4, 6];
+
+// Includes only return True or False
+console.log(arr.includes(5)); // false
+
+// IndexOf
+console.log(arr.indexOf(3)); // 2
+
+// LastIndexOf
+console.log(arr.lastIndexOf(6)); // 4
